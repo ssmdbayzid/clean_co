4c30b0fc2393617f583f07f8211b96bafb1158a8
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-12-14 00:51:54 +0600

    connection with server

--- src/pages/dashboard/AddServices.js
+++ src/pages/dashboard/AddServices.js
@@ -1,9 +1,37 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const AddServices = () => {
-  return (
+
+  const [service, setServices] = useState("")
+
+  useEffect(()=>{
+  /*  axios
+    .get("http://localhost:5000/services")
+    .then((res)=>setServices(res.data))
+  */
+
+// Immediate Function    
+ (async () =>{
+  const res = await axios.get("http://localhost:5000/services")
+  console.log(setServices(res.data)) 
+ })()
+  
+  },[])
+
+  // console.log(service); 
+  return (    
     <div>
-      <h1>Add Services</h1>      
+      {
+        service.map((s)=><div>
+          <h1>Service Name <span>{s.service}</span></h1>
+          <br />
+          <h3>Price: <span>{s.Price}</span></h3>
+        </div>)
+      }
+
       </div>
   )
 }
