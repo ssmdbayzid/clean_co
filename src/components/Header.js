8151ca6c1a58ec8229c9e3dd9792ead7969878c6
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-12-24 10:36:14 +0600

    complete 3 part c-session

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = ({children}) => {
       tabIndex={0} className="btn btn-ghost lg:hidden btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>}
-      <div className="flex-1 px-2 mx-2 text-2xl font-bold text-primary">Clean Co.
+      <div className="flex-1 px-2 mx-2 text-2xl font-bold text-primary corsur-pointer"><a href="/home">Clean Co</a> 
       </div>
       <div className="flex-none lg:hidden">
         <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
--- src/pages/dashboard/AddServices.js
+++ src/pages/dashboard/AddServices.js
@@ -1,41 +1,59 @@
 
+import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
-import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import fetcher from '../../api'
 
 const AddServices = () => {
 
-  const [service, setServices] = useState("")
-
+  const [imgURL, setImgURL] = useState("")
+  
   const {register, handleSubmit, reset} = useForm()
+  console.log(imgURL)
 
   
-  const addService = (data) =>{
-    console.log(data)
-    reset()
+  //* Form Submit -----
+  const onSubmit = async (data) =>{
+    const submitData = {...data, image: imgURL}
+    const res = await fetcher.post("service", submitData)
+    if(res){
+      reset();
+      setImgURL("")
+      console.log(res);
+    }
   }
+
+
+
+  //* Image upload in Imagebb
 
   const handleImgUpload = (event) =>{
     
-    const imgURL = event.target.files[0]
-
+    const image = event.target.files[0]
+    
     const formData = new FormData()
-
-    formData.set("image, imgURL")
-
-
-    console.log(imgURL);
+    
+    formData.set("image", image)
+    
+    axios.post("https://api.imgbb.com/1/upload?key=bd573eba9da05ba5bc027008889e81d8", formData)
+    .then((res)=>{  
+      setImgURL(res.data.data.display_url); 
+      setTimeout(console.log(imgURL), 1000)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })  
   }
+  
+  
 
-  // console.log(service); 
   return (    
     <div className="hero flex justify-center items-center mt-10 h-screen bg-accent">
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
         <h1 className='text-2xl text-primary font-bold text-center'>Add Service</h1>
-        <form onSubmit={handleSubmit(addService)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label className="label"
             htmlFor='serviceName'
@@ -43,6 +61,7 @@ const AddServices = () => {
             <span className="label-text">Service Name</span>
           </label>
           <input id='serviceName' type="text" placeholder="Service Name" className="input input-bordered"
+          required
           {...register("serviceName")}
           />
         </div>
@@ -53,26 +72,26 @@ const AddServices = () => {
             <span className="label-text">Service Charge</span>
           </label>
           <input type="text" placeholder="Service Charge" className="input input-bordered bg" 
-          id='serviceCharge'
+          id='serviceCharge' required
           {...register("serviceCharge")}
           />
 
         </div>
         <div className="form-control">
-          <label className="label"
-            htmlFor='imgURL'
+        <label className="label"
+            htmlFor='imgupload'
           >
-            <span className="label-text">Image Upload</span>
+            <span className="label-text">Upload Image</span>
           </label>
-          <input type="file" className="input input-bordered bg" 
-          id='imgURL' onChange={handleImgUpload}
-          // {...register("image")}
-
-          />
-
+      <input type="file"
+      id='imgupload'
+      className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
         </div>
+        
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Add Service</button>
+          <button disabled={!imgURL ? true : false}
+          className={!imgURL? "btn btn-primary" : " btn btn-primary"} 
+          >Add Service</button>
           
         </div>
         </form>
